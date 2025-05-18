da54e6006a0255290e21e52f8d939b7d545f649f
DynaJoshy <dynajo11@gmail.com>
2025-05-18 10:27:23 +0530

    initial commit

--- nextu/src/components/LandingPage.jsx
+++ nextu/src/components/LandingPage.jsx
@@ -5,14 +5,15 @@ const LandingPage = () => {
   const navigate = useNavigate();
 
   return (
-<div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-cyan-300 via-cyan-500 to-blue-900 text-white">      <div className="text-center">
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-cyan-300 to-cyan-900 text-white">
+      <div className="text-center">
         <img
           src="/logo.png"
           alt="App Logo"
           className="w-40 h-40 mb--8"
         />
         <h1 className="text-5xl font-extrabold mb-4 drop-shadow-lg">
-          Welcome to the Personality Test App
+          Career Preference Tests
         </h1>
         <p className="text-lg font-medium mb-8 max-w-md mx-auto drop-shadow-md">
           Discover your personality, career preferences, and learning styles in
@@ -20,10 +21,13 @@ const LandingPage = () => {
         </p>
         <button
           onClick={() => navigate("/register")}
-          className="px-8 py-4 bg-white text-blue-600 font-bold rounded-full shadow-lg hover:bg-blue-100 hover:scale-105 transition-transform duration-300"
+          className="px-8 py-4 bg-gradient-to-r from-white to-blue-500 white text-blue-600 font-bold rounded-full shadow-lg hover:bg-blue-100 hover:scale-105 transition-transform duration-300"
         >
           Get Started
         </button>
+        <p className="mt-8 text-sm font-semibold text-white drop-shadow-md ">
+          Powered by <footer className="text-yellow-300 italic pb-10">NextU Empower</footer>
+        </p>
       </div>
     </div>
   );
